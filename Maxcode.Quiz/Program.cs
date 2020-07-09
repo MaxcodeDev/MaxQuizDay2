d5b02bc62933743583756ab5a5fce2ce7e0caf53
Ion Dormenco <idormenco@gmail.com>
2020-07-09 11:33:15 +0300

    Update Program.cs

--- Maxcode.Quiz/Program.cs
+++ Maxcode.Quiz/Program.cs
@@ -7,6 +7,14 @@ namespace Maxcode.Quiz
 {
     class Program
     {
+        /*
+         * Maxquiz Olympics Day 2
+         * Do not change initializaion of DavidLetterman class
+         * You should add methods in DavidLetterman class only!
+         * Good luck!
+         * Submit your response here https://docs.google.com/forms/d/e/1FAIpQLScdpCWlr8IICuXmEwkzom7-A8tbMzat6uVbTtjkUpmDSlfWbg/viewform
+         * Read more about rules and prizes here https://www.facebook.com/notes/maxcode/maxquiz-rulebook-/3195266267201373/
+         */
         static void Main(string[] args)
         {
             var letters = new[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
@@ -24,12 +32,12 @@ namespace Maxcode.Quiz
     }
     class DavidLetterman : IEnumerable<char>
     {
-       private readonly List<char> _myLetters = new List<char>();
+        private readonly List<char> _myLetters = new List<char>();
         public IEnumerator<char> GetEnumerator() => _myLetters.GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
         /*
-         * Your code goes here:*
+         * Your code goes here
          */
     }
 }
